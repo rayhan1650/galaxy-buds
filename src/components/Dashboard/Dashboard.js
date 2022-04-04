d9edbde1ac766d875b166688c996c40968507e7e
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-04-04 22:31:22 +0600

    dashboard chart implemented

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  LineChart,
+  Line,
+} from "recharts";
 
 const Dashboard = () => {
+  const [businessData, setBusinessData] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/product-analysis-website/main/data.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setBusinessData(data));
+  }, []);
+
   return (
-    <div>
-      <h2>This is dashboard</h2>
+    <div className="d-flex flex-column flex-md-row justify-content-around">
+      <div>
+        <h3 className="text-primary">Month Wise Sell</h3>
+        <LineChart width={600} height={500} data={businessData}>
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+        </LineChart>
+      </div>
+      <div>
+        <h3 className="text-primary">Investment vs Revenue</h3>
+        <BarChart width={600} height={500} data={businessData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="investment" fill="#8884d8" />
+          <Bar dataKey="revenue" fill="#82ca9d" />
+        </BarChart>
+      </div>
     </div>
   );
 };
