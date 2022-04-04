c4935ddf8376994761264bb9bd2245694f3563cb
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-04-04 17:51:35 +0600

    dashboard component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <h1>Galaxy Buds</h1>
+      <Dashboard />
     </div>
   );
 }
