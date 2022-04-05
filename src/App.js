5b852cd4993af75a6b568d23f622a5fab89265db
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-04-05 12:58:50 +0600

    page not found implemented

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Blogs from "./components/Blogs/Blogs";
 import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import Reviews from "./components/Reviews/Reviews";
+import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
