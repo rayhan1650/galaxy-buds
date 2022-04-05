4a9dac52c90078aa426195af36c8f79a0886aad5
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-04-05 11:44:41 +0600

    react router installed

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -5,13 +5,9 @@ const Review = ({ reviewer }) => {
   const { name, rating, text, img } = reviewer;
   return (
     <div className="col">
-      <div className="card">
-        <div className="d-flex  align-items-center p-2">
-          <img
-            src={img}
-            className="card-img-top user-profile-pic"
-            alt="User profile"
-          />
+      <div className="card shadow">
+        <div className="d-flex bg-primary bg-opacity-10  align-items-center p-2">
+          <img src={img} className="user-profile-pic" alt="User profile" />
           <h5 className="card-title ms-3">{name}</h5>
         </div>
 
